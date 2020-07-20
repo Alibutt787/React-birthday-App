f12b162c44a0756edff3930f1731828745e518b6
Alibutt787 <67646902+Alibutt787@users.noreply.github.com>
2020-07-20 14:44:20 -0700

    Ahmad-birthday

---
+++ src/Ahmadbirth.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+
+ export function Cke(){
+  
+    
+    return(
+        <div>
+        
+        <h1><img className="by" src={require('./aq.png')} /> <b>Happy  Birthday<strong> AHMAD</strong></b> </h1>
+
+        <img  className="ht" src="https://i.pinimg.com/originals/e7/ef/ac/e7efac6ab709e5a5213198c83f076029.gif" />
+       <img id="fr" src="https://sweetlovemessages.com/wp-content/uploads/2019/10/1st_birthday_wishes_for_nephew.jpg" />
+        
+        </div>
+    )
+    }
