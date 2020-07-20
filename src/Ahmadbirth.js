75a1d7f7fba423fe380e6e2361aa46418865730b
Alibutt787 <67646902+Alibutt787@users.noreply.github.com>
2020-07-20 15:21:37 -0700

    image alt

--- src/Ahmadbirth.js
+++ src/Ahmadbirth.js
@@ -9,8 +9,8 @@ import React, { useState } from 'react';
         
         <h1><img className="by" src={require('./aq.png')} /> <b>Happy  Birthday<strong> AHMAD</strong></b> </h1>
 
-        <img  className="ht" src="https://i.pinimg.com/originals/e7/ef/ac/e7efac6ab709e5a5213198c83f076029.gif" />
-       <img id="fr" src="https://sweetlovemessages.com/wp-content/uploads/2019/10/1st_birthday_wishes_for_nephew.jpg" />
+        <img  className="ht" src="https://i.pinimg.com/originals/e7/ef/ac/e7efac6ab709e5a5213198c83f076029.gif" alt="HBD CAKE" />
+       <img id="fr" src="https://sweetlovemessages.com/wp-content/uploads/2019/10/1st_birthday_wishes_for_nephew.jpg" alt="Birthday Quote" />
         
         </div>
     )
