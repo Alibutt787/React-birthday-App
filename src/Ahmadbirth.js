528563ecce28e5a1aa23be1778abe638859f00b5
Alibutt787 <67646902+Alibutt787@users.noreply.github.com>
2020-07-20 15:27:31 -0700

    Update Ahmadbirth.js

--- src/Ahmadbirth.js
+++ src/Ahmadbirth.js
@@ -7,7 +7,7 @@ import React from'react';
     return(
         <div>
         
-        <h1><img className="by" src={require('./aq.png')} /> <b>Happy  Birthday<strong> AHMAD</strong></b> </h1>
+        <h1><img className="by" src={require('./aq.png')} alt="HBD BOY"/> <b>Happy  Birthday<strong> AHMAD</strong></b> </h1>
 
         <img  className="ht" src="https://i.pinimg.com/originals/e7/ef/ac/e7efac6ab709e5a5213198c83f076029.gif" alt="HBD CAKE" />
        <img id="fr" src="https://sweetlovemessages.com/wp-content/uploads/2019/10/1st_birthday_wishes_for_nephew.jpg" alt="Birthday Quote" />
