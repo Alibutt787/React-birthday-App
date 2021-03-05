6fd96fed79ed9113e42df92cb0f89fe1c36d67bd
AQIB Ali Butt <67646902+Alibutt787@users.noreply.github.com>
2021-03-05 10:56:05 +0500

    Update Ahmadbirth.js

--- src/Ahmadbirth.js
+++ src/Ahmadbirth.js
@@ -10,7 +10,7 @@ import React from'react';
         <h1><img className="by" src={require('./aq.png')} alt="HBD BOY"/> <b>Happy  Birthday<strong> AHMAD</strong></b> </h1>
 
         <img  className="ht" src="https://i.pinimg.com/originals/e7/ef/ac/e7efac6ab709e5a5213198c83f076029.gif" alt="HBD CAKE" />
-      <br/>  <img id="fr" src="https://sweetlovemessages.com/wp-content/uploads/2019/10/1st_birthday_wishes_for_nephew.jpg" alt="Birthday Quote" />
+      <br/>  <img id="fr" src="https://images.unsplash.com/photo-1518541335015-494e08dc3d8f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80" alt="Birthday Quote" />
         <footer>&copy; Copyright 2020 Aqib Butt</footer>
         </div>
     )
